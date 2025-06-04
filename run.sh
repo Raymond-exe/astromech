4e2592830fd186611e7bfc87450f6d8308f500ab
Raymond Wong <raymondwong1701@gmail.com>
2025-06-04 14:10:57 -0700

    Rename out dir to build dir

--- run.sh
+++ run.sh
@@ -6,16 +6,16 @@ echo "[run.sh] Checking for updates from remote repo..."
 git fetch --all
 git reset --hard origin/master
 
-echo "[run.sh] Cleaning ./out/ directory..."
-rm -rf out/*
-mkdir -p out
+echo "[run.sh] Cleaning build directory..."
+rm -rf build/*
+mkdir -p build
 
 echo "[run.sh] Compiling main file..."
-g++ src/main.cpp -o out/main
+g++ src/main.cpp -o build/main
 
 if [ $? -eq 0 ]; then
     echo "[run.sh] Succeeded compiling. Running program..."
-    ./out/main
+    ./build/main
 else
     echo "[run.sh] Failed to compile, check for errors!"
     echo "End of line."
