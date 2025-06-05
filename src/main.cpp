35d0040db31faaba48d0397090e492e1e3a233a2
Raymond Wong <raymondwong1701@gmail.com>
2025-06-05 00:52:31 -0700

    Servo control code

--- src/main.cpp
+++ src/main.cpp
@@ -9,25 +9,41 @@
 int pwmFrom(float, int);
 
 int main() {
-  PiPCA9685::PCA9685 pca{};
+    int fifo = open("/tmp/servo_ctrl", O_RDONLY);
+    if (fifo == -1) {
+        perror("open");
+        return 1;
+    }
 
-  pca.set_pwm_freq(60.0);
+    PiPCA9685::PCA9685 pca{};
+    pca.set_pwm_freq(60.0);
 
-  // for (int angle = 0; angle < 1000; angle++) {
-  //   pca.set_pwm(0, 0, angle);
-  //   std::cout << angle << "\n";
-  //   usleep(25'000);
-  // }
+    pca.set_pwm(0, 0, NEUTRAL); // channel, on, off
 
-  pca.set_pwm(0, 0, NEUTRAL);
+    int channel;
+    int direction;
+    float speed;
+
+    while (
+        read(fifo, &channel, sizeof(channel)) > 0 &&
+        read(fifo, &direction, sizeof(direction)) > 0 && 
+        read(fifo, &speed, sizeof(speed)) > 0
+    ) {
+        printf("Servo %d assigned direction: %d, speed: %f\n", channel, direction, speed);
+        pca.set_pwm(channel, 0, pwmFrom(speed, direction));
+    }
+
+    printf("Closing servo controller.\n");
+    close(fifo);
+    return 0;
 }
 
 int pwmFrom(float alpha, int direction) {
-  if (direction < -1) direction = -1;
-  if (direction > 1) direction = 1;
+    if (direction < -1) direction = -1;
+    if (direction > 1) direction = 1;
 
-  if (alpha > 1) alpha = 1;
-  if (alpha < 0) alpha = 0;
+    if (alpha > 1) alpha = 1;
+    if (alpha < 0) alpha = 0;
 
-  return round(alpha * PWM_SCALE * direction) + NEUTRAL;
+    return round(alpha * PWM_SCALE * direction) + NEUTRAL;
 }
