8e921968992d13cbf5a4bb7b9178291c8b8a051d
Raymond Wong <raymondwong1701@gmail.com>
2025-06-08 00:18:32 -0700

    disable audio streaming

--- run.sh
+++ run.sh
@@ -6,14 +6,14 @@ echo "[run.sh] Checking for updates from remote repo..."
 git fetch --all
 git reset --hard origin/master
 
-echo "[run.sh] Starting audio stream"
-(
-    while true; do
-        ffmpeg -f alsa -i hw:0 -ac 1 -f mp3 -content_type audio/mpeg -listen 1 http://localhost:8000
-        echo "Client disconnected, restarting stream in 1s..."
-        sleep 1
-    done
-) &
+# echo "[run.sh] Starting audio stream"
+# (
+#     while true; do
+#         ffmpeg -f alsa -i hw:0 -ac 1 -f mp3 -content_type audio/mpeg -listen 1 http://localhost:8000
+#         echo "Client disconnected, restarting stream in 1s..."
+#         sleep 1
+#     done
+# ) &
 
 echo "[run.sh] Starting flask server..."
 sudo python3 src/main.py
